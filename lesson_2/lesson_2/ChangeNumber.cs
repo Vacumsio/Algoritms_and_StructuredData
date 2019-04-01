6a9be4aa4f19938f9929af64f2e9318b8d0c1750
Alaska <vacumsio@gmail.com>
2019-04-01 17:13:36 +0300

    1st task

---
+++ lesson_2/lesson_2/ChangeNumber.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lesson_2
+{
+    class ChangeNumber
+    {
+
+        public string bin;
+        public void Per(int i)
+        {
+            if (i > 0)
+            {
+                if (i % 2 == 1)
+                    bin += "1";
+                else
+                    bin += "0";
+                Per(i / 2);
+            }
+        }
+    }
+}
