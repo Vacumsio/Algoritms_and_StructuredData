c7ca874c6e1630b3e68d2e49f31402a43868fde9
Alaska <vacumsio@gmail.com>
2019-04-01 17:35:39 +0300

    add

---
+++ lesson_2/lesson_2/NumPaw.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lesson_2
+{
+    class NumPaw
+    {
+        public double Pow(int value, int pow)
+        {
+            if (pow == 0)
+                return 1;
+            if (pow > 0)
+                return Pow(value, pow - 1) * value;
+            return 1.0 / Pow(value, -pow));
+        }
+    }
+}
