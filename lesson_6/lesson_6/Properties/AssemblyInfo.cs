16e850f1d6f081a05ce766de7dea72630ac15d02
Alaska <vacumsio@gmail.com>
2019-04-12 13:46:42 +0300

    start lesson_6

--- lesson_6/lesson_6/Properties/AssemblyInfo.cs
+++ lesson_6/lesson_6/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // Общие сведения об этой сборке предоставляются следующим набором
 // набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
 // связанные со сборкой.
-[assembly: AssemblyTitle("lesson_5")]
+[assembly: AssemblyTitle("lesson_6")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("lesson_5")]
+[assembly: AssemblyProduct("lesson_6")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
-[assembly: Guid("8d458350-515a-49fb-a857-d740ed553616")]
+[assembly: Guid("3204835e-bbc5-4be6-bff5-6aef513fd85c")]
 
 // Сведения о версии сборки состоят из следующих четырех значений:
 //
