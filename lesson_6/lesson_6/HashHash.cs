5d5c01261a6d2e16544d2085a8c0378afff6d827
Alaska <vacumsio@gmail.com>
2019-04-15 15:08:25 +0300

    adds

---
+++ lesson_6/lesson_6/HashHash.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace lesson_6
+{
+    class HashHash
+    {
+        public static void Hash()
+        {
+            string str = Console.ReadLine();
+            Console.WriteLine(str);
+
+            char[] hs = str.ToCharArray();
+            Console.WriteLine(hs);
+
+            Console.ReadKey();
+        }
+
+        Guid GetHashString(string s)
+        {
+            //переводим строку в байт-массим  
+            byte[] bytes = Encoding.Unicode.GetBytes(s);
+
+            //создаем объект для получения средст шифрования  
+            MD5CryptoServiceProvider CSP =
+                new MD5CryptoServiceProvider();
+
+            //вычисляем хеш-представление в байтах  
+            byte[] byteHash = CSP.ComputeHash(bytes);
+
+            string hash = string.Empty;
+
+            //формируем одну цельную строку из массива  
+            foreach (byte b in byteHash)
+                hash += string.Format("{0:x2}", b);
+
+            return new Guid(hash);
+        }
+    }
+}
---
+++ lesson_6/lesson_6/MainClass.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+/*
+1.Реализовать простейшую хэш-функцию. На вход функции подается строка, на выходе получается сумма кодов символов.
+2.Переписать программу, реализующее двоичное дерево поиска:
+    a.Добавить в него обход дерева различными способами.
+    b.Реализовать поиск в нём.
+*/
+
+namespace lesson_6
+{
+    class MainClass
+    {
+        public static int StringHashCode20(string value)
+        {
+            int num = 352654597;
+            int num2 = num;
+
+            for (int i = 0; i < value.Length; i += 4)
+            {
+                int ptr0 = value[i] << 16;
+                if (i + 1 < value.Length)
+                    ptr0 |= value[i + 1];
+
+                num = (num << 5) + num + (num >> 27) ^ ptr0;
+
+                if (i + 2 < value.Length)
+                {
+                    int ptr1 = value[i + 2] << 16;
+                    if (i + 3 < value.Length)
+                        ptr1 |= value[i + 3];
+                    num2 = (num2 << 5) + num2 + (num2 >> 27) ^ ptr1;
+                }
+            }
+
+            return num + num2 * 1566083941;
+        }
+
+        public static int StringHashCode40(string value)
+        {
+            int num = 5381;
+            int num2 = num;
+            for (int i = 0; i < value.Length; i += 2)
+            {
+                num = (((num << 5) + num) ^ value[i]);
+
+                if (i + 1 < value.Length)
+                    num2 = (((num2 << 5) + num2) ^ value[i + 1]);
+            }
+            return num + num2 * 1566083941;
+        }
+
+
+        static void Main(string[] args)
+        {
+            string value = "123";
+            Console.WriteLine(StringHashCode20(value));
+            Console.WriteLine(StringHashCode40(value));
+            Console.WriteLine(value.GetHashCode());
+
+            Console.ReadKey();
+        }
+    }
+}
