1a1c4002f72c5f0b068174306524b832d45521f0
Alaska <vacumsio@gmail.com>
2019-04-15 15:59:16 +0300

    adds

--- lesson_6/lesson_6/HashHash.cs
+++ lesson_6/lesson_6/HashHash.cs
@@ -17,7 +17,7 @@ namespace lesson_6
             Console.ReadKey();
         }
 
-        Guid GetHashString(string s)
+        public static Guid GetHashString(string s)
         {
             //переводим строку в байт-массим  
             byte[] bytes = Encoding.Unicode.GetBytes(s);
@@ -36,6 +36,45 @@ namespace lesson_6
                 hash += string.Format("{0:x2}", b);
 
             return new Guid(hash);
+        }
+
+        public static int StringHashCode20(string value)
+        {
+            int num = 352654597;
+            int num2 = num;
+
+            for (int i = 0; i < value.Length; i += 4)
+            {
+                int ptr0 = value[i] << 16;
+                if (i + 1 < value.Length)
+                    ptr0 |= value[i + 1];
+
+                num = (num << 5) + num + (num >> 27) ^ ptr0;
+
+                if (i + 2 < value.Length)
+                {
+                    int ptr1 = value[i + 2] << 16;
+                    if (i + 3 < value.Length)
+                        ptr1 |= value[i + 3];
+                    num2 = (num2 << 5) + num2 + (num2 >> 27) ^ ptr1;
+                }
+            }
+
+            return num + num2 * 1566083941;
+        }
+
+        public static int StringHashCode40(string value)
+        {
+            int num = 5381;
+            int num2 = num;
+            for (int i = 0; i < value.Length; i += 2)
+            {
+                num = (((num << 5) + num) ^ value[i]);
+
+                if (i + 1 < value.Length)
+                    num2 = (((num2 << 5) + num2) ^ value[i + 1]);
+            }
+            return num + num2 * 1566083941;
         }
     }
 }
