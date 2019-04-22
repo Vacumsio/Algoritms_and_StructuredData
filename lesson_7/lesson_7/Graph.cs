2004324405e63dc9aa8e4f795e0baf4421e4030d
Alaska <vacumsio@gmail.com>
2019-04-22 15:25:28 +0300

    ads

--- lesson_7/lesson_7/Graph.cs
+++ lesson_7/lesson_7/Graph.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using static GraphInDepth.MainClass;
 
 namespace GraphInDepth
 {
--- lesson_7/lesson_7/MainClass.cs
+++ lesson_7/lesson_7/MainClass.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-
 /*
  1.	Написать функции, которые считывают матрицу смежности из файла и выводят её на экран.
 2.	Написать рекурсивную функцию обхода графа в глубину.
@@ -36,6 +35,5 @@ namespace GraphInDepth
 
             Console.ReadKey();
         }
-
     }
 }
